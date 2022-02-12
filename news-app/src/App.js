130d0f317c03784560c264a06eca698d8337c78b
Wan Rong Joshua Wong <s10222829@connect.np.edu.sg>
2022-02-12 16:49:42 +0800

    Minor changes using bootstrap on nav & footer

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -5,6 +5,7 @@ import NewsList from './components/NewsList';
 function App() {
   return (
     <div className="App">
+        <a href="http://127.0.0.1:5500/Helpline.html">Reference</a>
         <NewsList/>
         {/*Here we call the News List, which pass in the props(parameter) to the NewsItem as data, to be filtered and designed as a newsItem for each article which call the func*/}
     </div>
