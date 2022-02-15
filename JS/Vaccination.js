a3df3d62aba56ff6868033490a82fac5a68f657e
TerrenceEng <S10223166@connect.np.edu.sg>
2022-02-16 03:31:41 +0800

    changed appointment to vaccination in header updated vaccination changed script.js name to vaccination.js

---
+++ JS/Vaccination.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $("#btn-check").click(function(e){
+        if ($("#overview").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#overview").css("display", "block");
+        }
+    });
+
+    $("#btn-check1").click(function(e){
+        if ($("#types").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#types").css("display", "block");
+        }
+    });
+
+    $("#btn-check2").click(function(e){
+        if ($("#pfizer").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#pfizer").css("display", "block");
+        }
+    });
+
+    $("#btn-check3").click(function(e){
+        if ($("#mordena").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#mordena").css("display", "block");
+        }
+    });
+
+    $("#btn-check4").click(function(e){
+        if ($("#jj").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#jj").css("display", "block");
+        }
+    });
+
+    $("#btn-check5").click(function(e){
+        if ($("#sideEffects").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#sideEffects").css("display", "block");
+        }
+    });
+
+    $("#btn-check6").click(function(e){
+        if ($("#sideEffects").css("display") == "none"){
+            $("article").css("display", "none");
+            $("#sideEffects").css("display", "block");
+        }
+    });
+})
+    
+    
+    
+   /*$("#btn-check2").click(function(e){
+        if ($("#mordena").css("display") == "none"){
+            $("#mordena").css("display", "block");
+        } else {
+            $("#mordena").css("display", "none");
+        };
+    })
+
+    $("#btn-check3").click(function(e){
+        if ($("#jj").css("display") == "none"){
+            $("#jj").css("display", "block");
+        } else {
+            $("#jj").css("display", "none");
+        };
+    })
+})*/
