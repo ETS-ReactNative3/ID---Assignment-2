75e5b5def685e220ef4d958f6f3ad7e29c40aee5
TerrenceEng <S10223166@connect.np.edu.sg>
2022-02-17 03:55:55 +0800

    added footer.html to link every page to updated the version of bootstrap used in all pages updated the vaccination.js updated vaccination.html added fonts and visibility class to style.css

--- JS/Vaccination.js
+++ JS/Vaccination.js
@@ -1,69 +1,49 @@
 $(document).ready(function(){
     $("#btn-check").click(function(e){
         if ($("#overview").css("display") == "none"){
-            $("article").css("display", "none");
+            $("article, table").hide();
             $("#overview").css("display", "block");
+            $("#dropdown").hide();
         }
     });
 
     $("#btn-check1").click(function(e){
         if ($("#types").css("display") == "none"){
-            $("article").css("display", "none");
-            $("#types").css("display", "block");
+            $("article").hide();
+            $("#types").show();
+            $("#dropdown").css({
+                "display":"flex",
+                "flex-direction":"column"
+            });
         }
     });
 
     $("#btn-check2").click(function(e){
         if ($("#pfizer").css("display") == "none"){
-            $("article").css("display", "none");
+            $("article, table").hide();
             $("#pfizer").css("display", "block");
         }
     });
 
     $("#btn-check3").click(function(e){
-        if ($("#mordena").css("display") == "none"){
-            $("article").css("display", "none");
-            $("#mordena").css("display", "block");
+        if ($("#moderna").css("display") == "none"){
+            $("article, table").hide();
+            $("#moderna").css("display", "block");
         }
     });
 
     $("#btn-check4").click(function(e){
         if ($("#jj").css("display") == "none"){
-            $("article").css("display", "none");
+            $("article, table").hide();
             $("#jj").css("display", "block");
         }
     });
 
     $("#btn-check5").click(function(e){
         if ($("#sideEffects").css("display") == "none"){
-            $("article").css("display", "none");
+            $("article, table").hide();
             $("#sideEffects").css("display", "block");
+            $("#dropdown").hide();
         }
     });
-
-    $("#btn-check6").click(function(e){
-        if ($("#sideEffects").css("display") == "none"){
-            $("article").css("display", "none");
-            $("#sideEffects").css("display", "block");
-        }
-    });
-})
-    
-    
-    
-   /*$("#btn-check2").click(function(e){
-        if ($("#mordena").css("display") == "none"){
-            $("#mordena").css("display", "block");
-        } else {
-            $("#mordena").css("display", "none");
-        };
-    })
-
-    $("#btn-check3").click(function(e){
-        if ($("#jj").css("display") == "none"){
-            $("#jj").css("display", "block");
-        } else {
-            $("#jj").css("display", "none");
-        };
-    })
-})*/
+});
